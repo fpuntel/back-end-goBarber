d0e7bfa5b64d1d25c42ab6cebf045b03ee33ace9
Fernando <fernandopuntel@gmail.com>
2020-08-13 17:58:23 -0300

    13-08

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,13 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+// describe para ficar organizado, divide os testes
+describe('CreateAppointments', () => {
+    // Nao criar nada no banco, para isso eh criado um repositorio fake.
+    it('should be able to create a new appointment', () => {
+        
+    });
+
+   /* it('should not be able to create two appointments on the same time', () => {
+        
+    });*/
+});
